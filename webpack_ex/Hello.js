9865ad084c31a2e469acb5d15733528a70454699
長谷川大耀 <hasehiro@hasegawadai-no-MacBook-Air.local>
2018-03-11 16:56:58 +0900

    PropTypesの追加

--- webpack_ex/Hello.js
+++ webpack_ex/Hello.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types'
+export default function Hello(props) {
+    return <h1>Hello! {props.name} </h1>;
+}
 
-export default function Hello(){
-    return <h1>Hello! webpack</h1>;
+Hello.propTypes={
+    name :PropTypes.string.isRequired
+}
+
+export const Greeting =(props)=>{
+return(    <div>
+      <div>  Greeting</div>
+        {props.children}
+        </div>
+)
+}
+
+export const HelloFragment = () => {
+    return (
+        <React.Fragment>
+            <div> こんにちは</div>
+            <div> Hello</div>
+        </React.Fragment>
+    )
 }
--- webpack_ex/entry.js
+++ webpack_ex/entry.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Hello from './Hello'
+import Hello, { Greeting } from './Hello'
+
+
+
 
 ReactDOM.render(
-<div>
-    <Hello />
-    <Hello />
-    <Hello />
-    </div>,
+
+    <Greeting>
+    <Hello name={"坂本龍馬"}/>
+    <Hello name={"西郷隆盛"}/>
+    </Greeting>,
     document.getElementById('root')
 )
 
