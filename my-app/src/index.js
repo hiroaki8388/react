5361bec6d7f78689e0b8a17ce3520d196b7e93db
hiroaki <>
2018-03-13 10:43:51 +0900

    reduxTODO(途中)

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,6 +3,68 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore} from 'redux'
+
+const initialState = {
+    task:"",
+    tasks: []
+}
+
+const inputTask=(task)=>({
+    type: 'INPUT_TASK',
+    payload:{
+        task
+    }
+
+})
+
+
+const addTask = (task) => ({
+    type: 'ADD_TASK',
+    payload: {
+        task
+    }
+})
+
+const store=createStore(tasksReducer)
+
+store.dispatch(addTask('Storeを学ぶ'))
+
+function tasksReducer(state=initialState,action){
+    switch(actiion.type){
+        case 'INPUT_TASK':
+        return{
+            ...state,
+            task:actiion.payload.task
+        }
+
+        case 'ADD_TASK':
+        return{
+            ...state,
+            tasks:state.tasks.concat([action.payload.task])
+        }
+
+        default:
+        return state
+    }
+}
+
+// この波括弧何?
+function TodoApp({store}){
+const {task,tasks}=store.getState()
+
+
+return(
+    <div>
+        <input type="text" onChange={(e)=>
+    store.dispatch(inputTask(e.target.value)) }/>
+
+    <input type="button" value="add" onClick={
+        ()=>store.dispatch(addTask(task))}/>
+
+</div>
+)
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
