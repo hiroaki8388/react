aa4c1edfc7ba9ae981d8bcc2ba09bf8e0543995a
hiroaki <>
2018-03-15 16:58:52 +0900

    ファイルの分割

---
+++ my-app/src/actions/tasks.js
@@ -0,0 +1,15 @@
+export const inputTask = (task) => ({
+    type: 'INPUT_TASK',
+    payload: {
+        task
+    }
+
+})
+
+
+export const addTask = (task) => ({
+    type: 'ADD_TASK',
+    payload: {
+        task
+    }
+})
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,77 +2,10 @@ import React from 'react';
 import './index.css';
 import { createStore, } from 'redux'
 import { render } from 'react-dom'
-
-const initialState = {
-    task: "",
-    tasks: []
-}
-
-const inputTask = (task) => ({
-    type: 'INPUT_TASK',
-    payload: {
-        task
-    }
-
-})
-
-
-const addTask = (task) => ({
-    type: 'ADD_TASK',
-    payload: {
-        task
-    }
-})
+import TodoApp from './components/TodoApp'
+import tasksReducer from './reducers/tasks'
 
 const store = createStore(tasksReducer)
-
-store.dispatch(addTask('Storeを学ぶ'))
-
-function tasksReducer(state = initialState, action) {
-    switch (action.type) {
-        case 'INPUT_TASK':
-            return {
-                ...state,
-                task: action.payload.task
-            }
-
-        case 'ADD_TASK':
-            return {
-                ...state,
-                tasks: state.tasks.concat([action.payload.task])
-            }
-
-        default:
-            return state
-    }
-}
-
-
-
-
-function TodoApp({ store }) {
-    const { task, tasks } = store.getState()
-
-
-    return (
-        <div>
-            <input type="text" onChange={(e) =>
-                store.dispatch(inputTask(e.target.value))} />
-
-            <input type="button" value="add" onClick={
-                () => store.dispatch(addTask(task))} />
-            <ul>
-                {
-                    tasks.map(function (item, i) {
-                        return (
-                            <li key={i}>{item}</li>
-                        )
-                    })
-                }
-            </ul>
-        </div>
-    )
-}
 
 function renderApp(store) {
     render(
